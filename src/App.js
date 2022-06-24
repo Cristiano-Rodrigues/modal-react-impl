13d531afd82c5fa8afc96090fc091093bc62eba0
Cristiano-Rodrigues <vladrodrigues2003@gmail.com>
2022-06-24 16:33:53 +0100

    chore: add a simple modal implementation with React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react'
 
-function App() {
+function Main() {
+  const [ visible, setVisible ] = useState(false)
+  
+  const showOrHideModal = () => {
+    setVisible(!visible)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <ModalButton setModalDispay={showOrHideModal}>Show modal</ModalButton>
+      <Modal setModalDispay={showOrHideModal} visible={visible}/>
+    </>
+  )
+}
+
+function Modal({ setModalDispay, visible }) {
+
+  return (
+    <div className={visible ? "container visible" : "container"}>
+      <div className="modal">
+        <ModalButton setModalDispay={setModalDispay}>Close button</ModalButton>
+      </div>
     </div>
   );
 }
 
-export default App;
+function ModalButton({ children, setModalDispay }) {
+  return (
+    <button
+      className="modal-btn"
+      onClick={setModalDispay}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default Main;
